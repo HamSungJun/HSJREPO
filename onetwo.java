a0461db34f419d569b9fdb2991d7870176e3b6fe
HamSungJun <wleo12345@naver.com>
2017-03-09 15:20:01 +0900

    one

---
+++ onetwo.java
@@ -0,0 +1,19 @@
+package OpenSource;
+
+import java.util.Scanner;
+
+public class onetwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner OT = new Scanner(System.in);
+		
+		int one = OT.nextInt();
+		
+		if(one == 1){
+			System.out.println("제 이름은 함성준입니다.");
+		}
+		
+	}
+
+}
